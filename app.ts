6c929e4eca6be5442dfc2e15757426bea68dd875
Great_Guy96 <58440950+GreatGuy96@users.noreply.github.com>
2022-03-09 08:45:52 -0500

    Fixed package.json

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+import { AddressInfo } from "express-ws/node_modules/@types/ws";
 import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
@@ -11,8 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+console.log(process.env);
+
 const listener = app.listen(3100, function () {
-  console.log("Listening on port " + listener.address().port);
+  console.log("Listening on port " + (listener.address() as AddressInfo).port);
 });
 
 app.use("", indexRouter);
