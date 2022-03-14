41922cdab8217a6c68ccdc34572b67a722461e22
Great_Guy96 <58440950+GreatGuy96@users.noreply.github.com>
2022-03-14 09:43:46 -0400

    Fix Env for Home, Fix Mongo Req

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,24 +1,29 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './secret.env' });
 import express from 'express';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const router = express.Router();
 
-console.log('index route');
 const uri = `mongodb+srv://great_guy96:${process.env.MONGO_PASSWORD}@baddle-dev.coexs.mongodb.net/test?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-	serverApi: ServerApiVersion.v1
+	serverApi: ServerApiVersion.v1,
 });
 
 router.get('/', async (req, res) => {
-	client.connect((err) => {
-		console.log(err);
+	try {
+		await client.connect();
 		const collection = client.db('test').collection('test_collection');
-		// perform actions on the collection object
-		console.log(collection);
 		console.log(collection.collectionName);
+		console.log(await collection.countDocuments());
 		client.close();
-	});
-	res.send({ hello: 'world' });
+	} catch (err) {
+		console.log(err);
+		res.send({ it: "didn't work" });
+		return;
+	}
+
+	res.send({ it: 'worked' });
 });
 
 export default router;
