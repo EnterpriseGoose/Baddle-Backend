68d4190126d9365f92a2c686b0aae08812e94537
Great_Guy96 <58440950+GreatGuy96@users.noreply.github.com>
2022-03-21 11:21:24 -0400

    Make Login Route

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -7,11 +7,11 @@ const router = express.Router();
 
 const uri = `mongodb+srv://great_guy96:${process.env.MONGO_PASSWORD}@baddle-dev.coexs.mongodb.net/test?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-	serverApi: ServerApiVersion.v1
+	serverApi: ServerApiVersion.v1,
 });
 
 router.post('/', async (req, res) => {
-	console.log(req);
+	console.log(req.body);
 	res.send(req.body);
 });
 
