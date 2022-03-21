77fcef8a71fc1b3505a949eaf7f325b0dbbc467e
Great_Guy96 <58440950+GreatGuy96@users.noreply.github.com>
2022-03-21 11:34:42 -0400

    Update Depdendencies

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 
 const uri = `mongodb+srv://great_guy96:${process.env.MONGO_PASSWORD}@baddle-dev.coexs.mongodb.net/test?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-	serverApi: ServerApiVersion.v1,
+	serverApi: ServerApiVersion.v1
 });
 
 router.get('/', async (req, res) => {
